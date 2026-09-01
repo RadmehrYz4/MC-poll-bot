2cac50248053fed9062edf0b75bf72b733dea040
RadmehrYz4 <radmehr.yazdani1390@gmail.com>
2026-09-01 21:27:16 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,16 @@
+import os
+import requests
 
+TOKEN = os.environ["BOT_TOKEN"]
+CHAT_ID = os.environ["CHAT_ID"]
+
+url = f"https://api.telegram.org/bot{TOKEN}/sendPoll"
+
+data = {
+    "chat_id": CHAT_ID,
+    "question": "🎮 امشب ماینکرفت بازی می‌کنید؟",
+    "options": '["✅ آره", "❌ نه"]',
+    "is_anonymous": False
+}
+
+requests.post(url, data=data)
